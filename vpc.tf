ebdadf55b9be316ee535af1503a2efa8beeeab1f
Harneet Singh <34583005+harneet-9039@users.noreply.github.com>
2021-05-27 14:54:46 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,14 @@
-module "network" {
-  source  = "app.terraform.io/db231/network/google"
-  version = "3.2.2"
+#module "network" {
+ # source  = "app.terraform.io/db231/network/google"
+ # version = "3.2.2"
   # insert required variables here
-  network_name = "mynw543"
-  project_id = "gcptraining"
-  subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
-}
+ # network_name = "mynw543"
+ # project_id = "gcptraining"
+ # subnets = [
+ # {
+  #  subnet_name   = "gaurav-subnet"
+  #  subnet_ip     = "10.100.10.0/24"
+  #  subnet_region = var.region
+#  }
+#]
+#}
