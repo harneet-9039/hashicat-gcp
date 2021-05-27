8e5b6c7618cb0a1d7819c936a2e6aa162895a7d0
Harneet Singh <34583005+harneet-9039@users.noreply.github.com>
2021-05-27 14:20:55 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network_example_basic_auto_mode" {
+  source  = "app.terraform.io/db231/network/google//examples/basic_auto_mode"
+  version = "3.2.2"
+  # insert required variables here
+  network_name = "mynw543"
+  project_id = "gcptraining"
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
